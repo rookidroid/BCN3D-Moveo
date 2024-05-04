90ecd99f58fb97b28fd5964ed830a35a1f0ba7fb
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-05-03 22:19:32 -0400

    Update moveo_arduino.ino

--- FIRMWARE/moveo_arduino/moveo_arduino.ino
+++ FIRMWARE/moveo_arduino/moveo_arduino.ino
@@ -1,5 +1,4 @@
-// Include the AccelStepper library:
-#include "AccelStepper.h"
+#include "FastAccelStepper.h"
 
 // Define stepper motor connections and motor interface type. 
 // Motor interface type must be set to 1 when using a driver:
@@ -15,17 +14,16 @@
 #define J4_DIR 33
 #define J4_STEP 32
 
-#define J5_DIR 35
-#define J5_STEP 34
+#define J5_DIR 15
+#define J5_STEP 2
 
-#define motorInterfaceType 1
+FastAccelStepperEngine engine = FastAccelStepperEngine();
 
-// Create a new instance of the AccelStepper class:
-AccelStepper j1_stepper;
-AccelStepper j2_stepper;
-AccelStepper j3_stepper;
-AccelStepper j4_stepper;
-AccelStepper j5_stepper;
+FastAccelStepper *j1_stepper = NULL;
+FastAccelStepper *j2_stepper = NULL;
+FastAccelStepper *j3_stepper = NULL;
+FastAccelStepper *j4_stepper = NULL;
+FastAccelStepper *j5_stepper = NULL;
 
 void setup() {
   pinMode(J1_DIR, OUTPUT);
@@ -39,100 +37,69 @@ void setup() {
   pinMode(J5_DIR, OUTPUT);
   pinMode(J5_STEP, OUTPUT);
 
-  j1_stepper = AccelStepper(motorInterfaceType, J1_STEP, J1_DIR);
-  j2_stepper = AccelStepper(motorInterfaceType, J2_STEP, J2_DIR);
-  j3_stepper = AccelStepper(motorInterfaceType, J3_STEP, J3_DIR);
-  j4_stepper = AccelStepper(motorInterfaceType, J4_STEP, J4_DIR);
-  j5_stepper = AccelStepper(motorInterfaceType, J5_STEP, J5_DIR);
-  
-  // Set the maximum speed and acceleration:
-  j1_stepper.setMaxSpeed(4000);
-  j1_stepper.setAcceleration(3000);
+  engine.init();
+  j1_stepper = engine.stepperConnectToPin(J1_STEP);
+  if (j1_stepper) {
+     j1_stepper->setDirectionPin(J1_DIR);
 
-  j2_stepper.setMaxSpeed(4000);
-  j2_stepper.setAcceleration(3000);
-  
-  j3_stepper.setMaxSpeed(4000);
-  j3_stepper.setAcceleration(3000);
-  
-  j4_stepper.setMaxSpeed(4000);
-  j4_stepper.setAcceleration(3000);
-  
-  j5_stepper.setMaxSpeed(4000);
-  j5_stepper.setAcceleration(3000);
+     j1_stepper->setSpeedInHz(3000);       // 500 steps/s
+     j1_stepper->setAcceleration(800);    // 100 steps/s²
+
+  }
+  j1_stepper->moveTo(1000, true);
+  j1_stepper->moveTo(-1000, true);
+  j1_stepper->moveTo(0, true);
+
+  j2_stepper = engine.stepperConnectToPin(J2_STEP);
+  if (j2_stepper) {
+     j2_stepper->setDirectionPin(J2_DIR);
+
+     j2_stepper->setSpeedInHz(3000);       // 500 steps/s
+     j2_stepper->setAcceleration(800);    // 100 steps/s²
+
+  }
+  j2_stepper->moveTo(2000, true);
+  j2_stepper->moveTo(-2000, true);
+  j2_stepper->moveTo(0, true);
+
+  j3_stepper = engine.stepperConnectToPin(J3_STEP);
+  if (j3_stepper) {
+     j3_stepper->setDirectionPin(J3_DIR);
+
+     j3_stepper->setSpeedInHz(3000);       // 500 steps/s
+     j3_stepper->setAcceleration(800);    // 100 steps/s²
+
+  }
+  j3_stepper->moveTo(2000, true);
+  j3_stepper->moveTo(-2000, true);
+  j3_stepper->moveTo(0, true);
+
+  j4_stepper = engine.stepperConnectToPin(J4_STEP);
+  if (j4_stepper) {
+     j4_stepper->setDirectionPin(J4_DIR);
+
+     j4_stepper->setSpeedInHz(3000);       // 500 steps/s
+     j4_stepper->setAcceleration(800);    // 100 steps/s²
+
+  }
+  j4_stepper->moveTo(2000, true);
+  j4_stepper->moveTo(-2000, true);
+  j4_stepper->moveTo(0, true);
+
+  j5_stepper = engine.stepperConnectToPin(J5_STEP);
+  if (j5_stepper) {
+     j5_stepper->setDirectionPin(J5_DIR);
+
+     j5_stepper->setSpeedInHz(3000);       // 500 steps/s
+     j5_stepper->setAcceleration(800);    // 100 steps/s²
+
+  }
+  j5_stepper->moveTo(2000, true);
+  j5_stepper->moveTo(-2000, true);
+  j5_stepper->moveTo(0, true);
+
 }
 
 void loop() {
-  // Set the target position:
-  j1_stepper.moveTo(2000);
-  // Run to target position with set speed and acceleration/deceleration:
-  j1_stepper.runToPosition();
 
-  delay(1000);
-
-  // Move back to zero:
-  j1_stepper.moveTo(0);
-  j1_stepper.runToPosition();
-
-  delay(1000);
-
-
-
-//  // Set the target position:
-//  j2_stepper.moveTo(2000);
-//  // Run to target position with set speed and acceleration/deceleration:
-//  j2_stepper.runToPosition();
-//
-//  delay(1000);
-//
-//  // Move back to zero:
-//  j2_stepper.moveTo(0);
-//  j2_stepper.runToPosition();
-//
-//  delay(1000);
-//
-//
-//  // Set the target position:
-//  j3_stepper.moveTo(2000);
-//  // Run to target position with set speed and acceleration/deceleration:
-//  j3_stepper.runToPosition();
-//
-//  delay(1000);
-//
-//  // Move back to zero:
-//  j3_stepper.moveTo(0);
-//  j3_stepper.runToPosition();
-//
-//  delay(1000);
-//
-//
-//
-//
-//  // Set the target position:
-//  j4_stepper.moveTo(2000);
-//  // Run to target position with set speed and acceleration/deceleration:
-//  j4_stepper.runToPosition();
-//
-//  delay(1000);
-//
-//  // Move back to zero:
-//  j4_stepper.moveTo(0);
-//  j4_stepper.runToPosition();
-//
-//  delay(1000);
-//
-//
-//
-//  // Set the target position:
-//  j5_stepper.moveTo(2000);
-//  // Run to target position with set speed and acceleration/deceleration:
-//  j5_stepper.runToPosition();
-//
-//  delay(1000);
-//
-//  // Move back to zero:
-//  j5_stepper.moveTo(0);
-//  j5_stepper.runToPosition();
-//
-//  delay(1000);
 }
